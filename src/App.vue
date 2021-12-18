768dc6fc94d0fafdae9d0df65f67c75a1f08965c
v.shamarinov <v.shamarinov@cbonds.info>
2021-12-18 15:41:13 +0300

    #0. Init App

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="glossy">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          icon="menu"
+        />
+
+        <q-toolbar-title>
+          MultiSocial
+        </q-toolbar-title>
+        <q-tabs align="left" shrink>
+          <q-route-tab :to="{ name: 'home'}"  label="Главная" />
+        </q-tabs>
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view></router-view>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'LayoutDefault',
+  setup () {
+    return {
+      leftDrawerOpen: ref(false)
+    }
+  }
+}
+</script>
